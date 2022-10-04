c5d4fe9adf7000088c44bf439ca2f14de2fae885
DevAgui <dagmartin98@gmail.com>
2022-10-04 19:25:14 +0200

    Added the directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HighlightDirective],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
---
+++ src/app/directives/highlight.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.appHighlight || this.defaultColor || 'yellow');
+  }
+  
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+  
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+  @Input() appHighlight = '';
+  @Input() defaultColor = '';
+
+  constructor(
+    private el: ElementRef) {
+    }
+
+}
